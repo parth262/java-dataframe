a5b1128f477e387909d786e3a78edbf955f60990
Parth Pandya <parthrpandya98@gmail.com>
2024-02-14 18:30:25 +0530

    Added concat test

--- lib/src/test/java/dataframe/DataFrameTests.java
+++ lib/src/test/java/dataframe/DataFrameTests.java
@@ -18,15 +18,16 @@ class DataFrameTests {
 
     DataFrameTests() {
         var data = List.of(
-            new Object[]{ 1, "parth", "pandya", true, "a/b/c" },
-            new Object[]{ 2, "arpan", "shah", true, "d/e/f" },
-            new Object[]{ 3, "ankit", "rathavi", false, "g/h/i" }
+                new Object[]{1, "parth", "pandya", true, "a/b/c"},
+                new Object[]{2, "arpan", "shah", true, "d/e/f"},
+                new Object[]{3, "ankit", "rathavi", false, "g/h/i"}
         );
-        var columns = new String[] {"id", "first_name", "last_name", "is_experienced", "path"};
+        var columns = new String[]{"id", "first_name", "last_name", "is_experienced", "path"};
         dataframe = DataFrame.create(data, columns);
     }
 
-    @Test void withColumnTest() {
+    @Test
+    void withColumnTest() {
         var dataframeCopy = dataframe.copy();
         dataframeCopy = dataframeCopy.withColumn("not_experienced", not("is_experienced"));
         dataframeCopy = dataframeCopy.withColumn("name", concat_ws(" ", "first_name", "last_name"));
@@ -37,47 +38,53 @@ class DataFrameTests {
         dataframeCopy = dataframeCopy.drop("is_experienced", "last_name");
 
         var expectedColumns = List.of(
-            "id", "first_name", "path", "not_experienced",
-            "name", "path_components", "dept", "created_at"
+                "id", "first_name", "path", "not_experienced",
+                "name", "path_components", "dept", "created_at"
         );
         assertTrue(dataframeCopy.getColumns().containsAll(expectedColumns));
     }
 
-    @Test void dropColumnsTest() {
-        var dropColumns = new String[] {"id", "first_name", "last_name"};
-        var remainingColumns = new String[] {"is_experienced", "path"};
+    @Test
+    void dropColumnsTest() {
+        var dropColumns = new String[]{"id", "first_name", "last_name"};
+        var remainingColumns = new String[]{"is_experienced", "path"};
         var newDataFrame = dataframe.drop(dropColumns);
         var newColumns = newDataFrame.getColumns().toArray(String[]::new);
         assertEquals(remainingColumns.length, newColumns.length);
         assertArrayEquals(remainingColumns, newColumns);
     }
 
-    @Test void selectColumnsTest() {
-        var selectColumns = new String[] {"id", "first_name", "last_name"};
+    @Test
+    void selectColumnsTest() {
+        var selectColumns = new String[]{"id", "first_name", "last_name"};
         var newDataFrame = dataframe.select(selectColumns);
         var newColumns = newDataFrame.getColumns().toArray(String[]::new);
         assertEquals(3, newColumns.length);
         assertArrayEquals(selectColumns, newColumns);
     }
 
-    @Test void emptyTest() {
+    @Test
+    void emptyTest() {
         var emptyDataFrame = DataFrame.empty();
         assertEquals(0, emptyDataFrame.getColumns().size());
         assertEquals("", emptyDataFrame.toString());
     }
 
-    @Test void copyTest() {
+    @Test
+    void copyTest() {
         var dataframeCopy = dataframe.copy();
         var dataframeString = dataframe.toString();
         var dataframeCopyString = dataframeCopy.toString();
         assertEquals(dataframeCopyString, dataframeString);
     }
 
-    @Test void countTest() {
+    @Test
+    void countTest() {
         assertEquals(3, dataframe.count());
     }
 
-    @Test void whereTest() {
+    @Test
+    void whereTest() {
         var isExperiencedFilter = ColumnTransformation.of(new Identity(), "is_experienced");
         var newDataFrame = dataframe.where(isExperiencedFilter);
         assertEquals(2, newDataFrame.count());
@@ -86,7 +93,8 @@ class DataFrameTests {
         assertEquals(1, newDataFrame2.count());
     }
 
-    @Test void joinTest() {
+    @Test
+    void joinTest() {
         var projectsData = List.of(
                 new Object[]{1, 1, "NCBI", "Data Engineer"},
                 new Object[]{2, 1, "Gogotech", "Data Engineer"},
@@ -105,18 +113,19 @@ class DataFrameTests {
         var projectIdForEmployee1 = joinedDataFrame
                 .where(Functions.isEquals("id", 1))
                 .select("project_id");
-        for(var row: projectIdForEmployee1.getRows()) {
+        for (var row : projectIdForEmployee1.getRows()) {
             assertNotNull(row[0]);
         }
         var projectIdForEmployee3 = joinedDataFrame
                 .where(Functions.isEquals("id", 3))
                 .select("project_id");
-        for(var row: projectIdForEmployee3.getRows()) {
+        for (var row : projectIdForEmployee3.getRows()) {
             assertNull(row[0]);
         }
     }
 
-    @Test void groupByTest() {
+    @Test
+    void groupByTest() {
         var groupedDataFrame = dataframe.groupBy("is_experienced");
         var isExperienced = dataframe.where(Functions.col("is_experienced"));
         var isNotExperienced = dataframe.where(Functions.not("is_experienced"));
@@ -133,15 +142,35 @@ class DataFrameTests {
         );
         var columns = new String[]{"id", "first_name", "last_name", "is_experienced", "path"};
         var expectedData = List.of(
-                new Object[]{ 1, "parth", "pandya", true, "a/b/c" },
-                new Object[]{ 2, "arpan", "shah", true, "d/e/f" },
-                new Object[]{ 3, "ankit", "rathavi", false, "g/h/i" },
+                new Object[]{1, "parth", "pandya", true, "a/b/c"},
+                new Object[]{2, "arpan", "shah", true, "d/e/f"},
+                new Object[]{3, "ankit", "rathavi", false, "g/h/i"},
                 new Object[]{4, "kruti", "mehta", false, "p/q/r"},
                 new Object[]{5, "deep", "erda", true, "m/n/o"}
         );
         var otherDataFrame = DataFrame.create(newData, columns);
         var expectedDF = DataFrame.create(expectedData, columns);
         var actualDF = dataframe.unionAll(otherDataFrame);
+        assertEquals(expectedDF, actualDF);
+    }
+
+    @Test
+    void concatTest() {
+        var newData = List.of(
+                new Object[] {"A,B"},
+                new Object[] {"A"},
+                new Object[] {"A"}
+        );
+        var columns = new String[]{"project"};
+        var expectedData = List.of(
+                new Object[]{1, "parth", "pandya", true, "a/b/c", "A,B"},
+                new Object[]{2, "arpan", "shah", true, "d/e/f", "A"},
+                new Object[]{3, "ankit", "rathavi", false, "g/h/i", "A"}
+        );
+        var expectedColumns =new String[]{"id", "first_name", "last_name", "is_experienced", "path", "project"};
+        var df = DataFrame.create(newData, columns);
+        var expectedDF = DataFrame.create(expectedData, expectedColumns);
+        var actualDF = dataframe.concat(df);
         assertEquals(expectedDF, actualDF);
     }
 }
